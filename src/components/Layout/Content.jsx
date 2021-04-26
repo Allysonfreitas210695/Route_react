0c07efc940246a0321e6afd9e09a8fc069403815
alisonfr <alisonfr83@gmail.com>
2021-04-26 10:51:07 -0300

    estruturado projeto

---
+++ src/components/Layout/Content.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Component.css"
+
+export default function Component() {
+  return <main className="Component"> Component Menu</main>;
+}
---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return( 
+  <div>
+    Component Home
+   <p>Bem-vindo!</p>
+  </div>
+  )
+}
